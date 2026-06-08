969be012cfa57cf66e8db16e46470fd3eb679e44
sudoRebel <mouad.scp.m@gmail.com>
2026-06-08 07:16:15 +0100

    Refactor: refactor messages look.

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main() {
     std::cout << "Enter a choice (1: Refresh, 2: Kill Process, 3: Exit): ";
 
     if (!(std::cin >> choice)) {
-      std::cout << "\nError: Invalid input. Please enter a number";
+      std::cout << "\nError: Invalid input. Please enter a number.\n";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       usleep(3000000);
@@ -26,14 +26,13 @@ int main() {
     }
 
     if (choice == 1) {
-      system("clear");
       continue;
     } else if (choice == 2) {
       int targetPid;
       std::cout << "Select process PID you want to kill: ";
 
       if (!(std::cin >> targetPid)) {
-        std::cout << "\nError: Invalid. Please enter PID id";
+        std::cout << "\nError: Invalid. Please enter PID id.\n";
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         usleep(3000000);
@@ -41,10 +40,11 @@ int main() {
       }
 
       if (kill(targetPid, SIGKILL) == 0) {
-        std::cout << "\nProcess killed";
+        std::cout << "\nOK: Process PID " << targetPid << "killed successfully.\n";
+        usleep(3000000);
         continue;
       } else {
-        perror("\nError: can't kill the process");
+        perror("\nError: can't kill the process.\n");
         usleep(3000000);
         continue;
       }
